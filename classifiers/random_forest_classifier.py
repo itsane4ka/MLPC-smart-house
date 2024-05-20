c973cec26e55e50bfeb193593b13048481e9e1c1
Yaroslav-Kapeliushniy <yarik.kapi1@gmail.com>
2024-05-20 19:44:38 +0200

    Modified Random forest classifier and trained svm classifier(added accuracy)

--- classifiers/random_forest_classifier.py
+++ classifiers/random_forest_classifier.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pandas as pd
+import pandas as pd 
 import torch
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
--- classifiers/svm_classifier.py
+++ classifiers/svm_classifier.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np 
 import pandas as pd
 import torch
 from sklearn.model_selection import train_test_split
